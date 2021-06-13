c25ea8606548d6bfcc35e50533cd447dc26ba82d
Shamiul haque <shamiulhaque1218@gmail.com>
2021-06-13 16:19:09 +0600

    5

--- Lab_03/Problem_05.php
+++ Lab_03/Problem_05.php
@@ -5,12 +5,13 @@
 $backColor="green"; $fontColor="red"; $imageWidth="80px"; $imageHeight="80px"; 
 
 ?> 
+<div class="1">
+  <div class="2" >
+ <img src="aiub_logo.jpg" width="$imageWidth" height="$imageHeight" />  </div>
+<div class="23">
+ <h2>American International University -<span style="background-color:$backColor;color:$fontColor"> 
 
-<img src="aiub_logo.jpg" width="300px" height="300px" /> 
-
-<h2>American International University -<span style="background-color:yellow;color:cyan"> 
-
-Bangladesh</h2> 
+Bangladesh</h2>  </div> </div>
 
 </span> 
       </body> 
