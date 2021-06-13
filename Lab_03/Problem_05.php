451409bf7590bcb09ce0612ad4a8b78d59b4a580
Shamiul haque <shamiulhaque1218@gmail.com>
2021-06-13 17:11:46 +0600

    Lab_03 code added

--- Lab_03/Problem_05.php
+++ Lab_03/Problem_05.php
@@ -8,7 +8,7 @@ $backColor="green"; $fontColor="red"; $imageWidth="80px"; $imageHeight="80px";
             <table>
                    <tr>
                         <td>
-                             <img src="aiub_logo.jpg" width-"imageWidth" height="imageHeight" />
+                             <img src="aiub_logo.jpg" width="imageWidth" height="imageHeight" >
                        </td>
                          <td>
                          <h2>American International University-<span style="background-color:backColor; color:fontColor"> Bangladesh </h2></span>
